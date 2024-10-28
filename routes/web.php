fc8705aae2cda0c62f79e7da7721d49417289f12
owen <muhamadowen02@gmail.com>
2024-10-28 16:10:13 +0700

    menambahkan user page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 
@@ -11,10 +12,6 @@ Route::get('/', function () {
 
 // Mengelompokkan rute backend
 Route::group(['prefix' => 'backend', 'as' => 'backend.'], function () {
-    Route::get('beranda', [BerandaController::class, 'berandaBackend'])
-        ->name('beranda')
-        ->middleware('auth');
-
     Route::get('login', [LoginController::class, 'loginBackend'])
         ->name('login');
 
@@ -23,4 +20,14 @@ Route::group(['prefix' => 'backend', 'as' => 'backend.'], function () {
 
     Route::post('logout', [LoginController::class, 'logoutBackend'])
         ->name('logout');
+
+    // Rute yang membutuhkan autentikasi
+    Route::middleware('auth')->group(function () {
+        Route::get('beranda', [BerandaController::class, 'berandaBackend'])
+            ->name('beranda');
+        
+        // Rute resource untuk UserController
+        Route::resource('user', UserController::class);
+    });
 });
+
